7fc857a0a90a9d7c7bfda80228c40d21a1d29544
Jamespahm <hoanggfff@gmail.com>
2024-03-17 00:15:41 +0700

    nnnn

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import { HeaderOnly } from '~/components/Layout';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Shop from '~/pages/Shop';
+import Search from '~/pages/Search';
 
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following, layout: null },
     { path: '/shop', component: Shop, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: HeaderOnly },
 ];
 const privateRoutes = [];
 
